b1d70a0ba529364a535e7df2d0c1deb19468b067
Ivan Diaz <idiazmiragaya294@gmail.com>
2026-04-27 16:41:17 +0200

    Spring boot core.

---
+++ src/main/java/com/obsidiangate/mc_panel/McPanelApplication.java
@@ -0,0 +1,13 @@
+package com.obsidiangate.mc_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obsidiangate/mc_panel/McPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obsidiangate.mc_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
