dec376983ff27886aaf05c996c1462069c3a1215
Manuel Fernandez <fernandezdebasamanuel@gmail.com>
2026-04-29 21:43:34 +0200

    feat: add compatibility to windows OS getting metrics (Ram)

--- src/main/java/com/obsidiangate/mcpanel/service/MetricsService.java
+++ src/main/java/com/obsidiangate/mcpanel/service/MetricsService.java
@@ -1,6 +1,7 @@
 package com.obsidiangate.mcpanel.service;
 
 import com.obsidiangate.mcpanel.dto.SystemMetricsDTO;
+import com.obsidiangate.mcpanel.util.system.OSUtil;
 import com.sun.management.OperatingSystemMXBean;
 import org.springframework.stereotype.Service;
 
@@ -13,35 +14,56 @@ public class MetricsService {
 
     public SystemMetricsDTO getSystemMetrics() {
         SystemMetricsDTO dto = new SystemMetricsDTO();
+        String OSName = OSUtil.getOSName();
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 
         try {
-            Process process = Runtime.getRuntime().exec("free -b");
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (line.startsWith("Mem:")) {
-                        // Split the line by white spaces
-                        String[] parts = line.split("\\s+");
+            long totalBytes;
+            long availableBytes;
+            long usedBytes;
+            double totalGB;
+            double usedGB;
 
-                        // 'free -b' structure:
-                        // [0] Mem: [1] Total [2] Used [3] Free [4] Shared [5] Buff/Cache [6] Available
-                        long totalBytes = Long.parseLong(parts[1]);
-                        long availableBytes = Long.parseLong(parts[6]);
-                        long usedBytes = totalBytes - availableBytes;
+            if (OSName.startsWith("lin")) {
+                Process process = Runtime.getRuntime().exec("free -b");
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.startsWith("Mem:")) {
+                            // Split the line by white spaces
+                            String[] parts = line.split("\\s+");
 
-                        double totalGB = totalBytes / 1073741824.0;
-                        double usedGB = usedBytes / 1073741824.0;
+                            // 'free -b' structure:
+                            // [0] Mem: [1] Total [2] Used [3] Free [4] Shared [5] Buff/Cache [6] Available
+                            totalBytes = Long.parseLong(parts[1]);
+                            availableBytes = Long.parseLong(parts[6]);
+                            usedBytes = totalBytes - availableBytes;
 
-                        dto.setRamTotal(Math.round(totalGB * 100.0) / 100.0);
-                        dto.setRamUsed(Math.round(usedGB * 100.0) / 100.0);
+                            totalGB = totalBytes / 1073741824.0;
+                            usedGB = usedBytes / 1073741824.0;
+
+                            dto.setRamTotal(Math.round(totalGB * 100.0) / 100.0);
+                            dto.setRamUsed(Math.round(usedGB * 100.0) / 100.0);
+                        }
                     }
                 }
+            } else if (OSName.startsWith("win")) {
+                totalBytes = osBean.getTotalMemorySize();
+                availableBytes = osBean.getFreeMemorySize();
+                usedBytes = totalBytes - availableBytes;
+
+                totalGB = totalBytes / 1073741824.0;
+                usedGB = usedBytes / 1073741824.0;
+
+                dto.setRamTotal(Math.round(totalGB * 100.0) / 100.0);
+                dto.setRamUsed(Math.round(usedGB * 100.0) / 100.0);
+
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         double cpu = osBean.getCpuLoad() * 100;
         dto.setCpuUsage(cpu < 0 ? 0 : Math.round(cpu * 100.0) / 100.0);
 
